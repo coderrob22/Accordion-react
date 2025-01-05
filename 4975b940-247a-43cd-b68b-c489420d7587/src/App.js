fc0417f3f5d9e39e7e0496a781e11962a42e54c0
coderrob22 <robertknott7@gmail.com>
2025-01-04 20:13:13 -0500

    accordion updates version 2

--- 4975b940-247a-43cd-b68b-c489420d7587/src/App.js
+++ 4975b940-247a-43cd-b68b-c489420d7587/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./styles.css";
 
 const faqs = [
@@ -29,16 +30,24 @@ export default function App() {
 function Accordion({ data }) {
   return( <div className="accordion">
     {data.map((c, i)=>(
-      <AccordionItem title={c.title} text={c.text} num={i} />
+      <AccordionItem title={c.title} text={c.text} num={i} key={c.title}/>
     ))}
   </div>
   );
 }
 function AccordionItem({num, title, text}){
-  return<div className="item">
-    <p className="number">{num}</p>
-    <p className="text">{title}</p>
-    <p className="icon">-</p>
-    <div className="content-box">{text}</div>
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle(){
+    setIsOpen((isOpen)=>!isOpen);
+  }
+
+  return(
+  <div className={ `item ${isOpen ? "open" : "" }`} onClick={handdleToggle}>
+    <p className="number">{num < 9? `0${num+1}`: num + 1 }</p>
+    <p className="title">{title}</p>
+    <p className="icon">{isOpen ? "-" : "+"}</p>
+    {isOpen && <div className="content-box">{text}</div>}
   </div>
+  )
 }
